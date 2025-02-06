20e34df039edeadf069be812271dd042b337764e
Autumn Pan <pan662607@gmail.com>
2025-02-06 02:23:12 +0000

    overhaul quaternion organization

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
+const { inverse, Quarternion, Vector3, AxisAngleToQuat } = require("./lib")
+
 module.exports = require("./lib")
+let p = new Vector3(0,1,0)
+console.log(p.rotate(Quarternion.fromAngleAxis(Math.PI/4, [0,0,1])))
