380281bd29904c9673e8553ff279389900988ccd
Autumn Pan <pan662607@gmail.com>
2025-02-06 02:58:31 +0000

    add complex number support

--- index.js
+++ index.js
@@ -1,5 +1,1 @@
-const { inverse, Quarternion, Vector3, AxisAngleToQuat } = require("./lib")
-
 module.exports = require("./lib")
-let p = new Vector3(0,1,0)
-console.log(p.rotate(Quarternion.fromAngleAxis(Math.PI/4, [0,0,1])))
